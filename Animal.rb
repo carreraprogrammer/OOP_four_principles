39ea9ecc615553cbb2c7efe4dd62596a3f990d55
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-31 09:54:27 -0500

    Add type

--- Animal.rb
+++ Animal.rb
@@ -1,9 +1,10 @@
 class Animal
-    def initialize(number_of_legs, name = "Unknown")
-      @id = Random.rand(1..1000)
-      @name = name
-      @number_of_legs = number_of_legs
-    end
+    def initialize(type, number_of_legs, name = "Unknown")
+        @id = Random.rand(1..1000)
+        @name = name
+        @number_of_legs = number_of_legs
+        @type = type
+      end
   
     def speak
       "Bla bla bla"
