00f01c68359795523d2c093c50d585fde86491ac
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-31 09:53:44 -0500

    Define Animal class

---
+++ Animal.rb
@@ -0,0 +1,17 @@
+class Animal
+    def initialize(number_of_legs, name = "Unknown")
+      @id = Random.rand(1..1000)
+      @name = name
+      @number_of_legs = number_of_legs
+    end
+  
+    def speak
+      "Bla bla bla"
+    end
+  end
+  
+  animal_1 = Animal.new(4, "Rex")
+  animal_2 = Animal.new(8)
+  
+  p animal_1.speak
+  p animal_2.speak
