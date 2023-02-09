0916724ca04130c67481cd82b2410cfcf9b4acb8
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-09 16:17:23 -0500

    Add method to remove legs

--- Animal.rb
+++ Animal.rb
@@ -1,3 +1,5 @@
+require "./remover.rb"
+
 class Animal
 
     attr_reader :id, :type, :number_of_legs
@@ -25,18 +27,52 @@ class Animal
           "www"
         end
       end
+
+      def remove_leg
+        remover = Remover.new()
+        @number_of_legs = remover.decrease(@number_of_legs)
+      end
     
 
   end
 
   require "./dog.rb"
   require "./spider.rb"
-  
+
+  animal = Animal.new("lion", 4, "Rex")  
   dog = Dog.new("black", "Rex")
   spider = Spider.new(85, "Wilma")
+
+  p animal
   
   p dog.bring_a_stick()
+  #=> "Here is your stick: ---------"
+
   p spider.bring_a_stick()
+  #=> nil
   
   p dog.make_a_web()
+  #=> nil
   p spider.make_a_web()
+  #=> "www"
+
+  p animal.number_of_legs
+  # => 4
+  p dog.number_of_legs
+  # => 4
+  p spider.number_of_legs
+  # => 4
+
+  p animal.remove_leg()
+  # => 3
+  p dog.remove_leg()
+  # => 3
+  p spider.remove_leg()
+  # => 7
+
+  p animal.remove_leg()
+  # => 2
+  p dog.remove_leg()
+  # => 2
+  p spider.remove_leg()
+  # => 2
